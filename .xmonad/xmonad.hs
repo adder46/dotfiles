0034fac124db6c7906619b9384072761eaa82abe
adder46 <dedmauz69@gmail.com>
2020-12-08 21:28:22 +0100

    Xmonad modMask fix

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -6,52 +6,125 @@ import XMonad.Hooks.ServerMode
 import XMonad.Hooks.SetWMName
 import XMonad.Layout.Gaps
 import XMonad.Layout.Spacing
-import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
 import XMonad.Util.Run (spawnPipe)
 import qualified XMonad.StackSet as W
+import qualified Data.Map as M
 
 myLayout = gaps [(U, 10), (R, 10), (L, 10), (D, 10)] $ spacingRaw True (Border 0 10 10 10) True (Border 10 10 10 10) True $
              layoutHook def
 
-myExtraWorkspaces = [(xK_0, "10"), (xK_minus, "11"), (xK_equal, "12")]
-
-myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
+myWorkspaces = 
+  [ (xK_1, "1")
+  , (xK_2, "2")
+  , (xK_3, "3")
+  , (xK_4, "4")
+  , (xK_5, "5")
+  , (xK_6, "6")
+  , (xK_7, "7")
+  , (xK_8, "8")
+  , (xK_9, "9")
+  , (xK_0, "10")
+  , (xK_minus, "11")
+  , (xK_equal, "12")
+  ]
 
 clickable' :: WorkspaceId -> String
 clickable' w = xmobarAction ("xmonadctl view\\\"" ++ w ++ "\\\"") "1" w
 
-myAdditionalKeys =
-    [ ((mod1Mask, key), windows $ W.greedyView ws)
-      | (key, ws) <- myExtraWorkspaces
+myKeys conf@(XConfig {XMonad.modMask = modMask}) = M.fromList $
+    [ ((modMask, key), windows $ W.greedyView ws)
+      | (key, ws) <- myWorkspaces
     ]
     ++
-    [ ((mod1Mask .|. shiftMask, key), windows $ W.shift ws)
-      | (key, ws) <- myExtraWorkspaces
+    [ ((modMask .|. shiftMask, key), windows $ W.shift ws)
+      | (key, ws) <- myWorkspaces
     ]
     ++
     [
-      ((mod1Mask, xK_F2), spawn "thunar")
-    , ((mod1Mask, xK_F3), spawn "firefox")
-    , ((mod1Mask, xK_F4), spawn "code")
-    , ((mod1Mask, xK_F5), spawn "thunderbird")
-    , ((mod1Mask, xK_Escape), spawn "xfce4-appfinder")
-    , ((mod4Mask, xK_Print), spawn "xfce4-screenshooter")
-    , ((mod4Mask, xK_KP_Add), spawn "amixer -D pulse sset Master 5%+")
-    , ((mod4Mask, xK_KP_Subtract), spawn "amixer -D pulse sset Master 5%-")
-    , ((mod1Mask .|. controlMask, xK_p), spawn "passmenu") 
-    , ((mod1Mask .|. controlMask, xK_c), spawn "clipmenu")
-    , ((mod1Mask .|. controlMask, xK_m), spawn "mailwatch_restart")
-    , ((mod1Mask .|. controlMask, xK_x), spawn "xfce4-panel -r")
-    , ((mod1Mask .|. controlMask, xK_Left), spawn "playerctl previous")
-    , ((mod1Mask .|. controlMask, xK_Right), spawn "playerctl next")
-    , ((mod1Mask .|. controlMask, xK_space), spawn "playerctl play-pause")
+    -- Spawn the terminal
+      ((modMask .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
+    
+    -- Close focused window 
+    , ((modMask .|. shiftMask, xK_c), kill)
+ 
+     -- Rotate through the available layout algorithms
+    , ((modMask, xK_space ), sendMessage NextLayout)
+ 
+    --  Reset the layouts on the current workspace to default
+    , ((modMask .|. shiftMask, xK_space), setLayout $ XMonad.layoutHook conf)
+ 
+    -- Resize viewed windows to the correct size
+    , ((modMask, xK_n), refresh)
+ 
+    -- Move focus to the next window
+    , ((modMask, xK_Tab), windows W.focusDown)
+ 
+    -- Move focus to the next window
+    , ((modMask, xK_j), windows W.focusDown)
+ 
+    -- Move focus to the previous window
+    , ((modMask, xK_k), windows W.focusUp)
+ 
+    -- Move focus to the master window
+    , ((modMask, xK_m), windows W.focusMaster)
+ 
+    -- Swap the focused window and the master window
+    , ((modMask, xK_Return), windows W.swapMaster)
+ 
+    -- Swap the focused window with the next window
+    , ((modMask .|. shiftMask, xK_j), windows W.swapDown)
+ 
+    -- Swap the focused window with the previous window
+    , ((modMask .|. shiftMask, xK_k), windows W.swapUp)
+ 
+    -- Shrink the master area
+    , ((modMask, xK_h), sendMessage Shrink)
+ 
+    -- Expand the master area
+    , ((modMask, xK_l), sendMessage Expand)
+ 
+    -- Push window back into tiling
+    , ((modMask, xK_t), withFocused $ windows . W.sink)
+ 
+    -- Increment the number of windows in the master area
+    , ((modMask, xK_comma), sendMessage (IncMasterN 1))
+ 
+    -- Deincrement the number of windows in the master area
+    , ((modMask, xK_period), sendMessage (IncMasterN (-1)))
+ 
+    -- toggle the status bar gap
+    , ((modMask, xK_b), sendMessage ToggleStruts)
+ 
+    -- Restart xmonad
+    , ((modMask,  xK_q), broadcastMessage ReleaseResources >> restart "xmonad" True)
+    ]
+
+myAdditionalKeysP =
+    [
+      ("M-<F2>", spawn "thunar")
+    , ("M-<F3>", spawn "firefox")
+    , ("M-<F4>", spawn "code")
+    , ("M-<F5>", spawn "thunderbird")
+    , ("M-<Escape>", spawn "xfce4-appfinder")
+    , ("M4-<Print>", spawn "xfce4-screenshooter")
+    , ("M4-<KP_Add>", spawn "amixer -D pulse sset Master 5%+")
+    , ("M4-<KP_Subtract>", spawn "amixer -D pulse sset Master 5%-")
+    , ("M-C-p", spawn "passmenu") 
+    , ("M-C-c", spawn "clipmenu")
+    , ("M-C-m", spawn "mailwatch_restart")
+    , ("M-C-x", spawn "xfce4-panel -r")
+    , ("M-C-<Left>", spawn "playerctl previous")
+    , ("M-C-<Right>", spawn "playerctl next")
+    , ("M-C-<Space>", spawn "playerctl play-pause")
     ]
 
 main = do
     xmproc <- spawnPipe "xmobar"
     xmonad $ docks def
         {
-          workspaces = myWorkspaces
+          workspaces = map show [1..12]
+          , keys = myKeys
           , borderWidth = 2
           , focusedBorderColor = "#226fa5"
           , normalBorderColor = "#191919"
@@ -70,5 +143,5 @@ main = do
             }
           , startupHook = setWMName "LG3D"
           , manageHook = manageDocks
-        } `additionalKeys` myAdditionalKeys
+        } `additionalKeysP` myAdditionalKeysP
 

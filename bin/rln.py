39fe51f550845cb514dcbd2ab3e6631548070a03
adder46 <dedmauz69@gmail.com>
2020-12-14 16:38:34 +0100

    Use a sane language to program tools

---
+++ bin/rln.py
@@ -0,0 +1,34 @@
+#!/home/alex/.venvs/env-rln/bin/python
+
+import pathlib
+import subprocess
+import click
+import click_option_group
+
+
+@click.command()
+@click.argument("src", type=click.Path(exists=True))
+@click.argument("dest", type=click.Path(exists=True))
+@click.option("-f", "--force", is_flag=True, required=False)
+@click_option_group.optgroup.group(
+    "Link type", cls=click_option_group.RequiredMutuallyExclusiveOptionGroup
+)
+@click_option_group.optgroup.option("-h", "--hard", is_flag=True)
+@click_option_group.optgroup.option("-s", "--soft", is_flag=True)
+def cli(src, dest, force, hard, soft):
+    options = [
+        x
+        for x in [
+            "ln",
+            "-s" if soft else "",
+            pathlib.Path(src).resolve(),
+            pathlib.Path(dest).resolve(),
+            "--force" if force else "",
+        ]
+        if x
+    ]
+    subprocess.run(options)
+
+
+if __name__ == "__main__":
+    cli()

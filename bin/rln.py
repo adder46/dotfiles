a46ae16c3e1d0d64cce4b7479ddfb86150c35dec
adder46 <dedmauz69@gmail.com>
2020-12-19 08:13:12 +0100

    Get rid of pathlib since click can resolve paths

--- bin/rln.py
+++ bin/rln.py
@@ -1,14 +1,13 @@
 #!/home/alex/.venvs/env-rln/bin/python
 
-import pathlib
 import subprocess
 import click
 import click_option_group
 
 
 @click.command()
-@click.argument("src", type=click.Path(exists=True))
-@click.argument("dest", type=click.Path())
+@click.argument("src", type=click.Path(exists=True, resolve_path=True))
+@click.argument("dest", type=click.Path(resolve_path=True))
 @click.option("-f", "--force", is_flag=True, required=False)
 @click_option_group.optgroup.group(
     "Link type", cls=click_option_group.RequiredMutuallyExclusiveOptionGroup
@@ -21,8 +20,8 @@ def cli(src, dest, force, hard, soft):
         for x in [
             "ln",
             "-s" if soft else None,
-            pathlib.Path(src).resolve(),
-            pathlib.Path(dest).resolve(),
+            src,
+            dest,
             "--force" if force else None,
         ]
         if x is not None
